8800142e06af0e5d8f6e4aa09c0245d236854a0e
Elur12 <m.klyshkin@ya.ru>
2024-06-03 17:33:37 +0300

    Add basemap

--- windows.py
+++ windows.py
@@ -7,18 +7,40 @@ import random
 import matplotlib
 matplotlib.use('Qt5Agg')
 
+from itertools import chain
+
 from PyQt5 import QtCore, QtWidgets
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+k = 1
+
+def draw_map(m, scale=0.2):
+    # draw a shaded-relief image
+    m.shadedrelief(scale=scale)
+    
+    # lats and longs are returned as a dictionary
+    lats = m.drawparallels(np.linspace(-90, 90, 13))
+    lons = m.drawmeridians(np.linspace(-180, 180, 13))
+
+    # keys contain the plt.Line2D instances
+    lat_lines = chain(*(tup[1][0] for tup in lats.items()))
+    lon_lines = chain(*(tup[1][0] for tup in lons.items()))
+    all_lines = chain(lat_lines, lon_lines)
+    
+    # cycle through these lines and set the desired style
+    for line in all_lines:
+        line.set(linestyle='-', alpha=0.3, color='w')
 
 class MplCanvas(FigureCanvas):
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = plt.figure(figsize=(8, 8))
-        m = Basemap(projection='ortho', resolution=None, lat_0=50, lon_0=-100)
-        m.bluemarble(scale=0.5)
+    def __init__(self, parent=None, width=8, height=6, dpi=100):
+        fig = plt.figure(figsize=(width, height), edgecolor='w')
+        m = Basemap(projection='cyl', resolution=None,
+            llcrnrlat=-90, urcrnrlat=90,
+            llcrnrlon=-180, urcrnrlon=180, )
+        draw_map(m)
         super(MplCanvas, self).__init__(fig)
 
 
@@ -27,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.canvas = MplCanvas(self, width=8, height=6, dpi=100)
         self.setCentralWidget(self.canvas)
 
         n_data = 50
@@ -39,16 +61,27 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Setup a timer to trigger the redraw by calling update_plot.
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(100)
+        self.timer.setInterval(10)
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
 
     def update_plot(self):
+        global k
+        m = Basemap(projection='cyl', resolution=None,
+            llcrnrlat=-90, urcrnrlat=90,
+            llcrnrlon=-180, urcrnrlon=180, )
+        m.scatter([37], [57], latlon=True,
+            c=np.log10([0.5]), s=[k],
+            cmap='Reds', alpha=0.5)
+        k = k + 100
+        draw_map(m)
         self.canvas.draw()
-        self.canvas
         
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     app.exec_()
+
+if __name__ == "__main__":
+    main()
